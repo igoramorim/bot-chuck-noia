46b3aada983979c979bbfcd72fcf0f67c2c811c0
igoramorim <igor340@gmail.com>
2019-02-02 15:56:41 -0200

    Removed test job

--- jobs.py
+++ jobs.py
@@ -3,9 +3,9 @@ from bot import create_tweet, teste
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
-def print_data():
-	teste()
+# @sched.scheduled_job('interval', minutes=1)
+# def print_data():
+# 	teste()
 
 @sched.scheduled_job('cron', day_of_week='mon-sun', hour='21-22', minute='0-59', timezone='America/Sao_Paulo')
 def update_a():
